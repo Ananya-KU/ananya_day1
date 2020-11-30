5084d6a6afdbb7d90a943d855f083624cfe132f8
Ananya K U <74902847+Ananya-KU@users.noreply.github.com>
2020-11-30 18:45:25 +0530

    Add files via upload

---
+++ error1.py
@@ -0,0 +1,4 @@
+#Syntax error
+#Error caused by not following the proper structure (syntax) of the language is called syntax error or parsing error.
+
+if a < 3
---
+++ error2.py
@@ -0,0 +1,4 @@
+#Logical error
+#Errors that occur at runtime (after passing the syntax test) are called exceptions or logical errors.
+
+1/0
